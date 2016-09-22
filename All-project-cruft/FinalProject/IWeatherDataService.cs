ebbaf737f6bd1913c5568025a0eec5ca1d82de7d
Yoav Saroya <stankovic100@gmail.com>
2016-09-22 21:39:12 +0300

    All the project files and any other cruft

---
+++ All-project-cruft/FinalProject/IWeatherDataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Weather
+{
+	/// <summary>
+	/// Represents a weather REStful web service. each class that implements it must
+	/// create WeatherData object with his own way.
+	/// </summary>
+	public interface IWeatherDataService
+	{
+		 WeatherData GetWeatherData(Location location);
+	}
+}
---
+++ All-project-cruft/FinalProjectTests/ApixuDataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Weather.DataServiceTests
+{
+	/// <summary>
+	/// Represents a test class that checks ApixuDataService works correctly.
+	/// </summary>
+	[TestClass()]
+	public class ApixuDataServiceTest
+	{
+		/// <summary>
+		/// Represents a test method that checks that ApixuData service actually
+		/// creates a WeatherData class with all correct data. if an exception
+		/// is thrown - the test is failed.
+		/// </summary>
+		[TestMethod()]
+		public void GetWeatherDataTest()
+		{
+			try
+			{
+				Location location = new Location("London");
+				ApixuDataService wd = ApixuDataService.GetInstance();
+				WeatherData actual = wd.GetWeatherData(location);
+				Assert.AreEqual("London",actual.Location.City);
+			}
+			catch(WeatherDataServiceException)
+			{
+				Assert.Fail();  //if an exception is caught, the test is fail.
+			}
+		}
+	}
+}
